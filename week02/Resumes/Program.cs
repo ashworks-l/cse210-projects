45cccccb0862388967c8df309361c1b41705b011
Ashley L <alujanyez@byupathway.edu>
2026-05-14 23:18:03 +0200

    sdfghjkl

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,66 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        // First job
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        // Second job
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        // Create resume
+        Resume myResume = new Resume();
+        myResume._name = "Allison Rose";
+
+        // Add jobs to the list
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        // Show resume
+        myResume.Display();
+    }
+}
+
+// Job class
+class Job
+{
+    public string _jobTitle;
+    public string _company;
+    public int _startYear;
+    public int _endYear;
+
+    public void Display()
+    {
+        Console.WriteLine(_jobTitle + " (" + _company + ") " + _startYear + "-" + _endYear);
+    }
+}
+
+// Resume class
+class Resume
+{
+    public string _name;
+
+    public List<Job> _jobs = new List<Job>();
+
+    public void Display()
+    {
+        Console.WriteLine("Name: " + _name);
+        Console.WriteLine("Jobs:");
+
+        foreach (Job job in _jobs)
+        {
+            job.Display();
+        }
     }
 }

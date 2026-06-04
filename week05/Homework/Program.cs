e284dacab87701acb6d25ee6788268dfaec85b45
ashworks-l <valash1330@gmail.com>
2026-06-04 23:28:36 +0200

    ASDFGHJKL

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        // TEST BASE CLASS
+        Assignment a1 = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(a1.GetSummary());
+
+        Console.WriteLine();
+
+        // TEST MATH ASSIGNMENT
+        MathAssignment math = new MathAssignment(
+            "Roberto Rodriguez",
+            "Fractions",
+            "7.3",
+            "8-19"
+        );
+
+        Console.WriteLine(math.GetSummary());
+        Console.WriteLine(math.GetHomeworkList());
+
+        Console.WriteLine();
+
+        // TEST WRITING ASSIGNMENT
+        WritingAssignment writing = new WritingAssignment(
+            "Mary Waters",
+            "European History",
+            "The Causes of World War II"
+        );
+
+        Console.WriteLine(writing.GetSummary());
+        Console.WriteLine(writing.GetWritingInformation());
     }
 }
